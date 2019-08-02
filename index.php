b7b529f4cc71d6e57b0d28f9799bedd0ea531fbb
nadeetsu <43735236+nadeetsu@users.noreply.github.com>
2019-08-02 10:39:18 +0700

    hello
    hai

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/fix.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/lib/font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Walk Around - Your Finest Footwear</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <header class="header">
+        <nav class="navbar navbar-expand-lg justify-content-center fixed-top">
+            <div class="container">
+                <a href="index.html" class="navbar-brand logo"> <img src="assets/img/logo/logos.svg" width="50px" height="50px" alt="wa" class="img-fluid"></a>
+                <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right">
+                    <i style="font-size: 20px;" class="fa fa-bars ml-2"></i>
+                </button>
+                <div id="navbarSupportedContent" class="collapse navbar-collapse justify-content-between align-items-center w-100">
+                    <ul class="navbar-nav mx-auto text-center">
+                        <li class="nav-item mr-2"> <a href="#about" class="nav-link">About</a></li>
+                        <li class="nav-item mr-2"> <a href="#inside" class="nav-link">What's Inside</a></li>
+                        <li class="nav-item mr-2"> <a href="#prd" class="nav-link">Products</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- landing pages -->
+    <section class="top-area section2">
+        <div class="heads_tx-area white">
+            <div class="area-bg"></div>
+            <div class="banner_area">
+                <div class="container">
+                    <div class="row flex-v-center">
+                        <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 wow fadeInLeft" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.2s">
+                            <div class="welcome-mockup center">
+                                <img class="img-fluid" src="assets/img/wijen.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 wow fadeInRight" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.2s">
+                            <div class="heads_tx">
+                                <h1>DON'T STOP RUN!</h1>
+                                <p class="text-lead mt-0 mb-0">Ciptakan Kaki yang nyaman dengan sepatu terbaik ini!</p>
+                                <div class="home-button">
+                                    <a href="#about">Discover More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="about">
+        <div class="container text-center text-lg-left">
+            <div class="row featurette align-items-center">
+                <div class="col-md-6 col-lg-6  mt-5 mt-lg-0 wow fadeInUp order-md-1" data-wow-duration="1s" data-wow-offset="10" data-wow-iteration="0.2s">
+                    <img src="assets/img/shoes.jpg" alt="" class="divider-image img-fluid">
+                </div>
+                <div class="col-md-6 col-lg-6 order-md-2 mt-5 mt-lg-0 wow fadeInUp">
+                    <div class="wow fadeInUp" data-wow-duration="1.2s" data-wow-offset="10" data-wow-iteration="0.4s">
+                        <h2 class="h2 g-title mb-2">About</h2>
+                        <div class="d-inline-block g-width-30 g-height-2 g-bg-primary mb-2"></div>
+                    </div>
+                    <div class="row wow fadeInUp" data-wow-duration="1.3s" data-wow-offset="10" data-wow-iteration="0.6s">
+                        <div class="col-lg-10">
+                            <p class="mt-3" style="font-size:1.25rem;font-weight:350; ">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut maiores eos sunt veniam quasi. Commodi dolor nesciunt qui sed culpa distinctio aliquam sunt odit, ar </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="team" class="content-section" style="background:#f9f9f9">
+        <div class="container">
+            <div class="row justify-content-center text-center g-mb-35 wow fadeInUp" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.2s">
+                <div class="col-lg-9">
+                    <h2 class="h2 g-title mb-2">Our Team</h2>
+                    <div class="d-inline-block g-width-30 g-height-2 g-bg-primary mb-2"></div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <!-- Team item -->
+                <div class="col-xl-4 col-lg-3 col-md-2 mb-4 wow fadeInUp" data-wow-duration="2s" data-wow-offset="10" data-wow-delay="1.2s" data-wow-iteration="0.6s">
+                    <div class="bg-white rounded shadow-sm py-4 px-3"><img src="assets/img/user2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                        <h5 class="mb-0">Nabila Triana</h5>
+                        <p class="small text-uppercase text-muted">CEO - Founder</p>
+                        <ul class="social mb-0 list-inline mt-3">
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-6 mb-4 wow fadeInUp" data-wow-duration="2s" data-wow-offset="10" data-wow-delay="1.3s" data-wow-iteration="0.8s">
+                    <div class="bg-white rounded shadow-sm py-4 px-3"><img src="assets/img/user1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                        <h5 class="mb-0">Reno Prayudha</h5>
+                        <p class="small text-uppercase text-muted">Co Founder</p>
+                        <ul class="social mb-0 list-inline mt-3">
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-6 mb-3 wow fadeInUp" data-wow-duration="2s" data-wow-offset="10" data-wow-delay="1.4s" data-wow-iteration="1s">
+                    <div class="bg-white rounded shadow-sm py-4 px-3"><img src="assets/img/user1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                        <h5 class="mb-0">Geraldy Lui</h5>
+                        <p class="small text-uppercase text-muted">Producer</p>
+                        <ul class="social mb-0 list-inline mt-3">
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-dribbble"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                            <li class="list-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- End -->
+            </div>
+        </div>
+    </section>
+    <section id="inside" class="features-area">
+        <div class="container">
+            <div class="row justify-content-center text-center g-mb-35 wow fadeInUp" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.2s">
+                <div class="col-lg-9">
+                    <h2 class="h2 g-title mb-2">What's Inside</h2>
+                    <div class="d-inline-block g-width-30 g-height-2 g-bg-primary mb-2"></div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <!-- Items -->
+                <div class="col-xl-4 col-sm-6 mb-5 wow fadeInLeft" data-wow-duration="2s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.7s">
+                    <div class="center-cont">
+                        <div class="media d-block d-md-flex text-center text-md-right g-mb-40">
+                            <div class="media-body mt-4 center-cont">
+                                <h4 class="h6 text-uppercase g-mb-15" style="font-weight:600; font-size:24px">Nice Material </h4>
+                                <p class="mb-0" style="font-weight:350; font-size:17px">Vestibulum vulputate lobortis tortor non tempus.</p>
+                            </div>
+                            <div class="d-md-flex center-cont g-mb-30 mb-4 g-ml-30--md">
+                                <span class="icon-inside u-icon-size--lg g-font-size-26 g-color-primary g-rounded-50x"><i class="fa fa-flask"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="media d-block d-md-flex text-center text-md-right g-mb-40">
+                            <div class="media-body mt-4 center-cont">
+                                <h4 class="h6 text-uppercase g-mb-15" style="font-weight:600; font-size:24px">Smell Reduction</h4>
+                                <p class="mb-0" style="font-weight:350; font-size:17px">Vestibulum vulputate lobortis tortor non tempus</p>
+                            </div>
+                            <div class="d-md-flex center-cont g-mb-30 mb-4 g-ml-30--md">
+                                <span class="icon-inside u-icon-size--lg g-font-size-26 g-color-primary g-rounded-50x"><i class="fa fa-cloud"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-6 mb-5 wow fadeInUp" data-wow-duration="2s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.6s">
+                    <div class="center-cont">
+                        <img src="assets/img/spatu.png" class="img-fluid img-responsive u-in-viewport" alt="sepatu">
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-6 mb-5 wow fadeInRight" data-wow-duration="2s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.7s">
+                    <div class="center-cont">
+                        <div class="media d-block d-md-flex text-center text-md-left g-mb-40">
+                            <div class="d-md-flex center-cont g-mb-30 mb-4 g-mr-30--md">
+                                <span class="icon-inside u-icon-size--lg g-font-size-26 g-color-primary g-rounded-50x"><i class="fa fa-resistance"></i></span>
+                            </div>
+                            <div class="media-body mt-4 center-cont">
+                                <h4 class="h6 text-uppercase mb-1" style="font-weight:600; font-size:24px">Elastic</h4>
+                                <p class="mb-0" style="font-weight:350; font-size:17px">Proin in ex blandit velit imperdiet tincidunt sit amet at quam.</p>
+                            </div>
+                        </div>
+
+                        <div class="media d-block d-md-flex text-center text-md-left">
+                            <div class="d-md-flex center-cont g-mb-30 g-mb-0--md g-mr-30--md mb-4">
+                                <span class="icon-inside u-icon-size--lg g-font-size-26 g-color-primary g-rounded-50x"><i class="fa fa-magic"></i></span>
+                            </div>
+
+                            <div class="mt-4 media-body center-cont">
+                                <h4 class="h6 text-uppercase mb-1" style="font-weight:600; font-size:24px">Modern STYLE</h4>
+                                <p class="mb-0" style="font-weight:350; font-size:17px">Proin tincidunt purus in tortor cursus fermentum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <section id="prd">
+        <div class="container">
+            <div class="row justify-content-center text-center g-mb-35 wow fadeInUp" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.2s">
+                <div class="col-lg-9">
+                    <h2 class="h2 g-title mb-2">Product</h2>
+                    <div class="d-inline-block g-width-30 g-height-2 g-bg-primary mb-2"></div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <!-- Team item -->
+                <div class="col-xl-4 col-sm-6 mb-4 wow fadeInUp" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1s" data-wow-iteration="0.4s">
+                    <a href="https://www.google.com/">
+                        <div class="parent" onclick="">
+                            <div class="child bg-one">
+                                <span>Casual</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-4 col-sm-6 mb-4 wow fadeInUp" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1.4s" data-wow-iteration="0.6s">
+                    <a href="https://www.google.com/">
+                        <div class="parent" onclick="">
+                            <div class="child bg-two">
+                                <span>Women</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-4 col-sm-6 mb-4 wow fadeInUp" data-wow-duration="1s" data-wow-offset="10" data-wow-delay="1.8s" data-wow-iteration="0.8s">
+                    <a href="https://www.google.com/">
+                        <div class="parent" onclick="">
+                            <div class="child bg-three">
+                                <span>Sporty</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="mail" class="parallax_mail">
+        <div class="container">
+            <div class="row justify-content-center text-center mb-4">
+                <div class="col-lg-9">
+                    <h2 class="h2 g-title mb-2">News Letter</h2>
+                    <div class="d-inline-block g-width-30 g-height-2 g-bg-primary mb-2"></div>
+                    <p class="mb-0" style="font-weight:450; font-size:17px">Subscribe to our newsletter for the latest news, promotions and deals.</p>
+                </div>
+            </div>
+            <div class="row justify-content-center text-center g-mb-35">
+                <div class="col-lg-4 col-md-2 col-sm-2">
+                    <div class="input-group u-shadow-v19 g-rounded-50 d-flex justify-content-center">
+                        <input class="form-ctrl g-color-gray-dark-v4 g-placeholder-gray-dark-v3 border-0 g-rounded-left-50 g-px-20" type="email" placeholder="Enter email" aria-label="Enter email">
+                        <div class="input-group-append g-bg-white border-0 g-rounded-right-50">
+                            <button class="favorite send_btn g-width-40 g-height-40 rounded-circle" type="submit">
+                                <i class="elm_icon fa fa-send"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 mt-2">
+                    <ul class="list-unstyled list-inline social text-center">
+                        <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-globe"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                    <p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">WalkAround</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="assets/lib/jquery/jquery.min.js"></script>
+    <script src="assets/lib/wow/js/wow.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
